8d4c35900a171b93f2593a2b70655ae24729ea3e
Natan Tibebu <natantibebu111@gmail.com>
2025-11-28 09:02:24 +0300

    Only the Interface no respose

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.css'
+import './Quizapp.css'
+import Quizapp from './Quizapp'
+
+function App() {
+
+  return (
+  <Quizapp/>
+  )
+}
+
+export default App
---
+++ src/Quizapp.jsx
@@ -0,0 +1,20 @@
+function Quizapp() {
+
+  return (
+     <div className="continer">
+        <h1>Quiz App</h1>
+        <hr/>
+        <h2>1.What is the capital of Japan?</h2>
+        <ul>
+            <li>Seoul</li>
+            <li>Tokyo</li>
+            <li>Beijing</li>
+            <li>Bangkok1111</li>
+        </ul>
+        <button>Next</button>
+        <div className="index">1 of 5 qustion</div>
+     </div>
+  )
+}
+
+export default Quizapp
